c6fe86e6de7528eacb4254d06c4a97b4c9274dc6
radkmb <rad.kmb@gmail.com>
2018-12-10 09:07:29 +0000

    validation

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -44,6 +44,10 @@ class TasksController extends Controller
      */
     public function store(Request $request)
     {
+        $this->validate($request, [
+            'content' => 'required|max:191'
+        ]);
+        
         $task = new Task;
         $task->content = $request->content;
         $task->save();
@@ -90,6 +94,10 @@ class TasksController extends Controller
      */
     public function update(Request $request, $id)
     {
+        $this->validate($request, [
+            'content' => 'required|max:191',
+        ]);
+        
         $task = Task::find($id);
         $task->content = $request->content;
         $task->save();
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
     </head>
     
     <body>
+        @include('commons.error_messages')
         @yield('content')
     </body>
 </html>
